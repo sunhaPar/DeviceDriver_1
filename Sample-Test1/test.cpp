ba6559ad260daca2a824a8de7c16b133d3f89ea2
DONGTAN2\user <sunha0710@naver.com>
2024-06-03 13:54:19 +0900

    app 구현

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #pragma once
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+
 #include "../Project1/DeviceDriver.cpp"
+#include "../Project1/Application.cpp"
 
 using namespace testing;
 using namespace std;
@@ -18,6 +21,16 @@ public:
 	MockFlashMemoryDevice fakeFlash;
 	DeviceDriver deviceDriver{ &fakeFlash };
 };
+
+
+class AppTestFixture : public testing::Test {
+public:
+	MockFlashMemoryDevice fakeFlash;
+	DeviceDriver deviceDriver{ &fakeFlash };
+	//MockApplication app{ &deviceDriver };
+	Application app{ &deviceDriver };
+};
+
 
 TEST_F(DriverTestFixture, Read5timesCheck) {
 	EXPECT_CALL(fakeFlash, read(0x1000)).Times(5);
@@ -52,6 +65,8 @@ TEST_F(DriverTestFixture, WritePass) {
 		.WillOnce(Return(0xFF))
 		.WillRepeatedly(Return(0xAA));
 
+	EXPECT_CALL(fakeFlash, write(0x30, 0xAA));  //write도 한번 호출될거 라고 알려주는 거임
+
 	deviceDriver.write(0x30, 0xAA);
 
 	EXPECT_EQ(0xAA, deviceDriver.read(0x30));
@@ -65,4 +80,37 @@ TEST_F(DriverTestFixture, WriteFail) {
 	EXPECT_THROW(deviceDriver.write(0x30, 0xAA), WriteFailException);
 }
 
+TEST_F(AppTestFixture, ReadChunkTest) {
 
+	EXPECT_CALL(fakeFlash, read(_))
+		.Times((0x50+1)*5)
+		.WillRepeatedly(Return(0xAA));
+
+	app.ReadAndPrint(0x00, 0x50);
+}
+
+
+TEST_F(AppTestFixture, WriteChunkTest) {
+
+	EXPECT_CALL(fakeFlash, read(_))
+		.WillRepeatedly(Return(0xFF));
+
+	app.WriteAll(0xAA);
+
+	EXPECT_CALL(fakeFlash, read)
+		.Times((Write_End+1) * 5)
+		.WillRepeatedly(Return(0xAA));
+
+	app.ReadAndPrint(Write_Start, Write_End);
+}
+
+
+
+TEST_F(AppTestFixture, WriteChunkTestFail) {
+
+	EXPECT_CALL(fakeFlash, read(_))
+		.WillRepeatedly(Return(0xAA));
+
+	EXPECT_THROW(app.WriteAll(0xAA), WriteFailException);
+
+}
